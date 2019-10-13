295d78fefa0c120d65ea5d4252b143444c915853
jacelaquerre <Colchester31>
2019-10-13 13:28:29 -0400

    initial commit

---
+++ CPUscheduler.c
@@ -0,0 +1,17 @@
+//
+// Created by jacel on 10/13/2019.
+//
+
+#include "CPUscheduler.h"
+
+Process *createProcess() {
+
+}
+
+void enqueueProcess(PQueueNode **eventPQueue, Process *processes, int numProcesses) {
+
+}
+
+void runSimulation(int schedulerType, int quantum, PQueueNode *eventPQueue) {
+
+}
---
+++ CPUscheduler.h
@@ -0,0 +1,39 @@
+//
+// Created by jacel on 10/13/2019.
+//
+
+#ifndef ASSINGMENT_3_CPUSCHEDULER_H
+#define ASSINGMENT_3_CPUSCHEDULER_H
+
+typedef struct PQNodestruct {
+    int priority;
+    void *data;
+    struct PQueueStruct *next;
+} PQueueNode;
+
+typedef struct {
+    int pid;
+    int burstTime;
+    int waitTime;
+    int numPreemptiions;
+    int lastTime;
+} Process;
+
+typedef enum EventTypeEnum {
+    PROCESS_SUBMITTED,
+    PROCESS_STARTS,
+    PROCESS_ENDS,
+    PROCESS_TIMESLICE_EXPIRES
+} EventType;
+
+typedef struct {
+    EventType eventType ;
+    Process *process ;
+} Event;
+
+Process *createProcess();
+void enqueueProcess(PQueueNode **eventPQueue, Process *processes, int numProcesses);
+void runSimulation(int schedulerType, int quantum, PQueueNode *eventPQueue);
+
+
+#endif //ASSINGMENT_3_CPUSCHEDULER_H
